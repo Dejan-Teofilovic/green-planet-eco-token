dbed3bc9092b9fdcade1591ef1a9011643302531
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-04-20 22:01:41 +0900

    token created

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const { API_KEY, MAINNET_RPC_URL, TESTNET_RPC_URL, WALLET_PRIVATE_KEY } = process.env;
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  solidity: "0.8.19",
+  defaultNetwork: 'goerli',
+  networks: {
+    goerli: {
+      url: TESTNET_RPC_URL,
+      accounts: [WALLET_PRIVATE_KEY],
+    },
+    ethereum: {
+      url: MAINNET_RPC_URL,
+      accounts: [WALLET_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: API_KEY,
+  },
+};
---
+++ scripts/run.js
@@ -0,0 +1,37 @@
+//  Current contract address: 0x464Efc7d5B4ACc96a09d8DfC6Ce6f90cC1F39acB
+
+const { utils } = require("ethers");
+
+async function main() {
+  const baseTokenURI = "https://ipfs.io/ipfs/QmNyDfNaWYAF8EJwi53HfkThxDkXUVRXj5pTZWgFph2z4j/";
+
+  // Get owner/deployer's wallet address
+  const [owner] = await hre.ethers.getSigners();
+
+  // Get contract that we want to deploy
+  const contractFactory = await hre.ethers.getContractFactory("ECO");
+
+  // Deploy contract with the correct constructor arguments
+  // const contract = await contractFactory.deploy(baseTokenURI);
+  const contract = await contractFactory.deploy();
+
+  // Wait for this transaction to be mined
+  await contract.deployed();
+
+  // Get contract address
+  console.log("Contract deployed to:", contract.address);
+
+  // // Mint 3 NFTs by sending 0.03 ether
+  // txn = await contract.mint(1, { value: utils.parseEther("0.01") });
+  // await txn.wait();
+  // console.log("1 NFT is minted successfully, ok?");
+  // // Get all token IDs of the owner
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
